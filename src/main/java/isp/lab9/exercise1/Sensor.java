1bc16fff026d1b50b929e61c81148282cb10af2f
Mihai Hulea <59961959+automatica-cluj@users.noreply.github.com>
2020-04-24 21:09:07 +0300

    Update exercises.

--- src/main/java/isp/lab9/exercise1/Sensor.java
+++
@@ -1,8 +0,0 @@
-package isp.lab9.exercise1;
-
-import java.beans.PropertyChangeListener;
-import java.util.Random;
-
-public abstract class Sensor implements PropertyChangeListener {
-    protected Random random = new Random();
-}
